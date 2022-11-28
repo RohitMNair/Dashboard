39fdb38af7f1449bc2e8d1d779dd2aa41ff0be78
SurajSahoo2016 <142202016@smail.iitpkd.ac.in>
2022-11-28 13:11:27 +0530

    page2

--- Dashboard/pages/page2.py
+++ Dashboard/pages/page2.py
@@ -20,20 +20,38 @@ HOST = 'localhost'
 #Initializing SqlAlchemy Postgresql Db Instance
 db = create_engine("postgresql+psycopg2://postgres:123@localhost:5432/popular_movies")
 
-
-select_query_stmnt_year = text( "select year_released \
-                            from movies\
-                        where year_released is not NULL\
-                        group by year_released \
-                        order by year_released asc;")
+select_query_stmnt_genre = text( "select genre \
+                            from genres\
+                        where genres is not NULL ")
 connection = db.connect()
-result_yr = connection.execute(select_query_stmnt_year)
-df = pd.DataFrame(result_yr.fetchall(),columns=result_yr.keys())
-arr = df.year_released.values
+result_gen = connection.execute(select_query_stmnt_genre)
+df = pd.DataFrame(result_gen.fetchall(),columns=result_gen.keys())
+arr = df.genre.values
+
+###################################
 
 
-
-option = st.selectbox('select the year you want the find the 10 best movies wrt ratings', (arr))
+option = st.selectbox('select the genre you want to find best tag of', (arr))
 st.write('You selected:', option)
+
+genre_ = str(option)
+
+
+number = st.number_input('Enter the number of top tags to find')
+st.write('You chose ', number)
+
+
+
+select_query_stmnt_tag_gen = text( "select ge.genre, tg.tag, count(tg.tag) \
+                        from genres as ge, tags as tg, movie_genres as mg\
+                        where ge.genre_Id= mg.genre_Id and mg.movie_Id=tg.movie_Id and ge.genre=\'" + str(genre_) + 
+                        "\' group by ge.genre,tg.tag\
+                        order by count(tg.tag) DESC\
+                        limit " + str(number) +  ";")
+connection = db.connect()
+result_tg = connection.execute(select_query_stmnt_tag_gen)
+df = pd.DataFrame(result_tg.fetchall(),columns=result_tg.keys())
+
+
 
 st.table(df)
