0160e2f7f95a2afcf9eeb35f424ca2b449482cd8
Rohit Nair <nukebomindia@gmail.com>
2022-11-28 19:05:40 +0530

    added best movie query

--- Dashboard/Main.py
+++ Dashboard/Main.py
@@ -7,7 +7,7 @@ import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 import numpy as np
-
+from wordcloud import WordCloud
 #Defining Db Credentials
 USER_NAME = 'postgres'
 PASSWORD = '123'
@@ -27,7 +27,51 @@ with st.sidebar:
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
 
-#######################################
+
+##########################################################
+with db.connect() as connection:
+
+    best_movie = connection.execute(text(
+        "select m.movie_id as movie_id, m.title as title \
+        from movies m, ratings r \
+        where m.movie_id = r.movie_id \
+        group by m.movie_id \
+        order by count(r.rating) DESC, avg(r.rating) desc \
+        limit 1;"
+    ))
+    df = pd.DataFrame(best_movie.fetchall(),columns=best_movie.keys())
+    st.write("The best movie is ", df["title"].values[0])
+##########################################################
+select_query_stmnt_year = text( "select year \
+                            from movies\
+                        where year is not NULL\
+                        group by year \
+                        order by year asc;")
+
+with db.connect() as connection:
+    result_yr = connection.execute(select_query_stmnt_year)
+    df = pd.DataFrame(result_yr.fetchall(),columns=result_yr.keys())
+    arr = df.year.values
+    option = st.selectbox('select the year you want the find the 10 best movies wrt ratings', (arr))
+    st.write('You selected:', option)
+
+####################################
+
+    year = str(option)
+
+
+    new_query = "select title from movies as mo where movie_id in \
+                    ( select rt.movie_id  from ratings as rt where movie_id in\
+                    (select movie_id from movies as mv where year = " + " " + year + " " + ") group by rt.movie_id order by avg(rating) desc limit 10)"
+
+    result_yr = connection.execute(text(new_query))
+    df = pd.DataFrame(result_yr.fetchall(),columns=result_yr.keys())
+    arr = df.title.values
+
+    st.table(df)
+
+#######################################################################################
+
 movie_name = st.text_input("Enter movie name you want the find the 5 most relevant tags")
 if str(movie_name).find('\'')>=0:
     str_n = list(movie_name)
@@ -38,94 +82,53 @@ if str(movie_name).find('\'')>=0:
 else:
     movie_name = "\'%"+str(movie_name)+"%\'"
 
-connection = db.connect()
-matched_mnames = connection.execute(text(
-    f"select title\
-    from movies\
-    where title ilike {movie_name}"
-))
-df = pd.DataFrame(matched_mnames.fetchall(),columns=matched_mnames.keys())
-arr = df["title"].values
+with db.connect() as connection:
+    matched_mnames = connection.execute(text(
+        f"select title\
+        from movies\
+        where title ilike {movie_name}"
+    ))
+    df = pd.DataFrame(matched_mnames.fetchall(),columns=matched_mnames.keys())
+    arr = df["title"].values
 
 
-option1 = st.selectbox('select the movie you want the find the 5 most relevant tags', (arr))
-st.write('You selected:', option1)
+    option1 = st.selectbox('select the movie', (arr))
+    st.write('You selected:', option1)
 
-if str(option1).find('\'')>=0:
-    str_n = list(option1)
-    str_n.insert(option1.find("'"),'\\')
-    option1 = ''.join(str_n)
-    option1 = "E\'"+str(option1)+"\'"
+    if str(option1).find('\'')>=0:
+        str_n = list(option1)
+        str_n.insert(option1.find("'"),'\\')
+        option1 = ''.join(str_n)
+        option1 = "E\'"+str(option1)+"\'"
 
-else:
-    option1 = "\'"+str(option1)+"\'"
+    else:
+        option1 = "\'"+str(option1)+"\'"
 
-select_query_stmnt = text("select gt.tag as tag,gs.relevance as relevance\
-                        from genome_tags as gt, genome_scores as gs \
-                        where gs.movie_id = (select mv.movie_id \
-					    from movies as mv \
-					    where title = " + option1 + " ) and gs.tag_id = gt.tag_id \
-                        ORDER BY gs.relevance DESC LIMIT 5;")
-
-
-result_2 = connection.execute(select_query_stmnt)
-df = pd.DataFrame(result_2.fetchall(),columns=result_2.keys())
-
-st.table(df)
-st.bar_chart(df['relevance'])
+    select_query_stmnt = text("select gt.tag as tag,gs.relevance as relevance\
+                            from genome_tags as gt, genome_scores as gs \
+                            where gs.movie_id = (select mv.movie_id \
+                            from movies as mv \
+                            where title = " + option1 + " ) and gs.tag_id = gt.tag_id \
+                            ORDER BY gs.relevance DESC LIMIT 5;")
 
 
-dict_ = dict(zip(df.tag,df.relevance))
+    result_2 = connection.execute(select_query_stmnt)
+    df = pd.DataFrame(result_2.fetchall(),columns=result_2.keys())
+
+    st.table(df)
+    st.bar_chart(df['relevance'])
 
 
-from wordcloud import WordCloud
+    dict_ = dict(zip(df.tag,df.relevance))
 
+    try:
+        wc = WordCloud().fit_words(dict_)
+        st.image(wc.to_array())
+    except ValueError:
+        # Prevent the error from propagating into your Streamlit app.
+        st.write("Nothing to show.... No tags for that particular movie")
 
-
-try:
-    wc = WordCloud().fit_words(dict_)
-    st.image(wc.to_array())
-except ValueError:
-  # Prevent the error from propagating into your Streamlit app.
-  st.write("Nothing to show.... No tags for that particular movie")
-
-
-##########################################################
-
-select_query_stmnt_year = text( "select year \
-                            from movies\
-                        where year is not NULL\
-                        group by year \
-                        order by year asc;")
-connection = db.connect()
-result_yr = connection.execute(select_query_stmnt_year)
-df = pd.DataFrame(result_yr.fetchall(),columns=result_yr.keys())
-arr = df.year.values
-
-
-option = st.selectbox('select the year you want the find the 10 best movies wrt ratings', (arr))
-st.write('You selected:', option)
-
-####################################
-
-
-year = str(option)
-
-
-new_query = "select title from movies as mo where movie_id in \
-                ( select rt.movie_id  from ratings as rt where movie_id in\
-                 (select movie_id from movies as mv where year = " + " " + year + " " + ") group by rt.movie_id order by avg(rating) desc limit 10)"
-
-connection = db.connect()
-result_yr = connection.execute(text(new_query))
-df = pd.DataFrame(result_yr.fetchall(),columns=result_yr.keys())
-arr = df.title.values
-
-st.table(df)
-
-#######################################################################################
-
-
+############################################################################
 
 
 def main_page():
