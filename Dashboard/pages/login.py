36207a72522e47e154c0a46f03d97073e40be01f
Rohit Nair <nukebomindia@gmail.com>
2022-12-06 19:14:43 +0530

    created a login page

--- Dashboard/pages/login.py
+++ Dashboard/pages/login.py
@@ -1,7 +1,6 @@
-import numpy as np
+import hashlib
 import pandas as pd
 import streamlit as st
-import hashlib
 from sqlalchemy.engine import create_engine
 from sqlalchemy.sql import text
 #Defining Db Credentials
@@ -15,33 +14,54 @@ HOST = 'localhost'
 #Initializing SqlAlchemy Postgresql Db Instance
 db = create_engine("postgresql+psycopg2://postgres:123@localhost:5432/popular_movies")
 
-def make_hashes(password):
-	return hashlib.sha256(str.encode(password)).hexdigest()
+def make_hashes(str_to_hash):
+    """
+    function to hash a string
+    params: str_to_hash (string)
+    returns hashed string
+    """
+    return hashlib.sha256(str.encode(str_to_hash)).hexdigest()
 
-def check_hashes(password,hashed_text):
-	if hashed_text == password:
-		return True
-	return False
+def check_hashes(hash_to_match,str_to_hash_n_match):
+    """
+    function to check 2 strings match or not
+    params: hash_to_match (string), str_to_hash_n_match(string)
+    returns: bool
+    """
+    if make_hashes(str_to_hash_n_match) == hash_to_match:
+        return True
+    return False
+
 choice = st.selectbox("Login or SignUp!",["Login","SignUp"])
 with db.connect() as connection:
-    username = st.text_input("Username")
-    password = st.text_input("Password",type="password")
-    
     if choice == "Login":
-        details = connection.execute(text(
+        username = st.text_input(label = "Username", value="")
+        password = st.text_input(label = "Password",value = "" ,type="password")
+        if st.button("Login!"):
+            details = connection.execute(text(
+                f"select username, password from usertable where username = '{username}';"
+            ))
+            df = pd.DataFrame(details.fetchall(),columns=details.keys())
+            actual_pass = df["password"].values[0]
+            if check_hashes(actual_pass, password):
+                st.session_state["logged_in"] = True
+                st.write("You are logged in")
+            else:
+                st.session_state["logged_in"] = False
+                st.write("You are not logged in")
+    elif choice == "SignUp":
+        username = st.text_input(label = "Username", value="")
+        password = st.text_input(label = "Password",value = "" ,type="password")
+        existing_details = connection.execute(text(
             f"select username, password from usertable where username = '{username}';"
         ))
-        df = pd.DataFrame(details.fetchall(),columns=details.keys())
-        actual_pass = df["password"].values[0]
-        if check_hashes(actual_pass, make_hashes(password)):
-            st.session_state["logged_in"] = True
-            st.write("You are logged in")
-        else:
-            st.session_state["logged_in"] = False
-            st.write("YOu are not logged in")
-    elif choice == "SignUp":
-        connection.execute(text(
-            f"INSERT INTO usertable (username, password) VALUES ('{username}','{make_hashes(password)}');"
-        ))
-        st.write("User created and logged in")
-        st.session_state["logged_in"] = True
+        df = pd.DataFrame(existing_details.fetchall(),columns=existing_details.keys())
+        if st.button("SignUp!"):
+            if len(df.username) > 0:
+                st.write("Username already exists")
+            else:
+                connection.execute(text(
+                    f"INSERT INTO usertable (username, password) \
+                        VALUES ('{username}','{make_hashes(password)}');"
+                    ))
+                st.write("User created")
